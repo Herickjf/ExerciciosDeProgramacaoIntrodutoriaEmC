58bd43f3ffe26f78d0ab521fb8c3dbafb0463c44
Herickjf <125290993+Herickjf@users.noreply.github.com>
2023-08-15 09:01:07 -0300

    Add files via upload

---
+++ media.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+
+int main() 
+{
+	
+	int n;
+	printf("How many grades do ya have?\n");
+	scanf("%d", &n);
+	
+	float x[n], media;
+	for(int i=0; i < n; i++)
+	{
+		printf("Type a grade:\n");
+		scanf("%f", &x[i]);
+		
+		media = media + x[i];
+	}
+	
+	media = media / n;
+	
+	printf("Your average is equals %f\n", media);
+	return 0;
+	
+}
