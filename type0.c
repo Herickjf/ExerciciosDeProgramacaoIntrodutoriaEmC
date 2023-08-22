d29763aab72794e62efe6d43c70e783521706704
Herickjf <125290993+Herickjf@users.noreply.github.com>
2023-08-22 08:30:19 -0300

    type0.c

--- type0.c
+++ type0.c
@@ -5,7 +5,7 @@ int main () {
 	int n, sum=0, q=0, bigger, smaller;
 	float average = 0;
 	
-	printf("Lemme explain it to ya. Since you dont type 0, Ill still asking you for a hole number. When you do type 0, Ill show ya the sum, average, bigger and smaller of the numbers u typed. So, type a number:\n");
+	printf("Lemme explain it to ya. Since you dont type 0, Ill still asking you for a hole number. When you do type 0, Ill show ya the sum and average of the numbers u typed. So, type a number:\n");
 	scanf("%i", &n);
 	
 	while( n!= 0) {
