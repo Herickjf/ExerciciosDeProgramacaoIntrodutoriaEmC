62ef786492bfd782195c164b2f36f9967640d1df
Herickjf <125290993+Herickjf@users.noreply.github.com>
2023-11-08 16:19:17 -0300

    Update secoundstohours.c

--- basico /secoundstohours.c
+++ basico /secoundstohours.c
@@ -5,20 +5,15 @@
 int main () {
     //definir variáeis
     int  s, hr, min, sec;
-
     s = -1;
-
     //Coleta de dados
      while (s < 0) {
-
         printf("Digite um tempo em segundos: \n");
         scanf("%d", &s);
         if (s < 0)
         {
                 printf("Ops, o tempo em segundos que voce informou eh invalido. Tente novamente com um numero maior que 0.\n");
         }
-
-
      }
 
     //Fazer os calculos
@@ -26,10 +21,7 @@ int main () {
     min = (s % 3600) / 60;
     sec = (s % 3600) % 60;
 
-
-
     //Devolver o resultado
-    printf("Muito bem, o tempo em segundos que voce informou equivale a %d horas, %d minutos e %d segundos!", hr, min, sec);
-
+    printf("Muito bem, o tempo em segundos que voce informou equivale a %d hrs, %d min e %d s!", hr, min, sec);
     return 0;
 }
