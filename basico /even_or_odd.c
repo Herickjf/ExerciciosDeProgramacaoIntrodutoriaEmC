e9161214bd85ee3fb11188b5ae050e07644948fd
Herickjf <125290993+Herickjf@users.noreply.github.com>
2023-11-08 16:02:58 -0300

    Update even_or_odd.c

--- basico /even_or_odd.c
+++ basico /even_or_odd.c
@@ -4,16 +4,12 @@ int main() {
 	int n;
 	printf("Type a number?\n");
 	scanf("%d", &n);
-
 	if(n!=0)
-		if( n%2 == 0){
+		if( n%2 == 0)
 			printf("%d is even.", n);
-		}
 		else 
 			printf("%d is odd.", n);
 	else 
 		printf("%d is neither even nor odd", n);
-	
-	return 0;
-	
+	return 0;	
 }
